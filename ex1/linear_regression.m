e2f7b7fc8ebfb40ffe60a91ac66fd91aa68982a1
SatoshiLiang <chihchengliang@gmail.com>
2014-11-11 21:39:32 +0800

    ex1a linear regression done

--- ex1/linear_regression.m
+++ ex1/linear_regression.m
@@ -7,8 +7,8 @@ function [f,g] = linear_regression(theta, X,y)
   %   y - The target value for each example.  y(j) is the target for example j.
   %
   
-  m=size(X,2);
-  n=size(X,1);
+  m=size(X,2); % 400
+  n=size(X,1); % 14
 
   f=0;
   g=zeros(size(theta));
@@ -22,3 +22,9 @@ function [f,g] = linear_regression(theta, X,y)
   %        computed gradient in 'g'.
   
 %%% YOUR CODE HERE %%%
+
+
+  for i = 1:m
+    f += (theta' * X(:,i)- y(i))^2;
+    g += X(:,i)*(theta' * X(:,i)- y(i));
+  endfor
